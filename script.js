32fd2dad86220f1bef7aa17150d6c987d60abf0b
qxnx <144263402+qxnx@users.noreply.github.com>
2025-12-24 01:00:58 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,38 @@
-<script src="https://cdn.jsdelivr.net/npm/ethers@5.7.2/dist/ethers.umd.min.js"></script>
-
-<script>
 const connectButton = document.getElementById('connectButton');
 const accountInfo = document.getElementById('accountInfo');
 
 if (typeof window.ethereum !== 'undefined') {
-    console.log('MetaMask установлен!');
+    console.log('установлен!');
 
     // Создаем провайдер Ethers.js
     const provider = new ethers.providers.Web3Provider(window.ethereum);
 
-    // Слушаем событие подключения кошелька
-    connectButton.addEventListener('click', async () => {
-        try {
-            // Запрос доступа к аккаунтам
-            await window.ethereum.request({ method: 'eth_requestAccounts' });
+    => {
+        // Слушаем событие подключения кошелька
+        connectButton.addEventListener('click', async () {
+            try {
+                await window.ethereum.request({
+                    method: 'eth_requestAccounts'
+                });
 
-            const signer = provider.getSigner();
-            const address = await signer.getAddress();
-            const balance = await provider.getBalance(address);
+                const signer = provider.getSigner();
+                const address = await signer.getAddress();
+                const balance = await provider.getBalance(address);
 
-            accountInfo.innerHTML = `
-                <p>Адрес кошелька: ${address}</p>
-                <p>Баланс (в Wei): ${balance.toString()}</p>
-                <p>Баланс (в Ether): ${ethers.utils.formatEther(balance)}</p>
-            `;
-        } catch (error) {
-            console.error('Ошибка при подключении:', error);
-        }
-    });
+                accountInfo.innerHTML = `
+                    <p>Адрес кошелька: ${address}</p>
+                    <p>Баланс (в Wei): ${balance.toString()}</p>
+                    <p>Баланс (в Ether): ${ethers.utils.formatEther(balance)}</p>
+                `;
+            } catch (error) {
+                console.error(
+                    "Ошибка при подключении:",
+                    error
+                );
+            }
+        });
+    }
 
 } else {
     accountInfo.innerHTML = '<p>Пожалуйста, установите MetaMask.</p>';
 }
-</script>
